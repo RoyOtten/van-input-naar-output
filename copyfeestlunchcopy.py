cdfde9c37abc7588d35b7525e43dbf1690943d11
RoyOtten <99065498@mydavinci.nl>
2021-09-08 11:05:37 +0200

    6e commmit

---
+++ copyfeestlunchcopy.py
@@ -0,0 +1,17 @@
+croissantjes = 0.39
+croissantjesaantal = int(input( "hoeveel croissantjes heb je?"))
+aantal = 17 
+stokbroden = 2.78
+stokbrodenaantal = int(input( "hoeveel stokbroodjes heb je?"))
+korting = 0.50
+kortingsbonnen= int(input("hoeveel korting heb je"))
+antwoord = int((croissantjes * aantal) + (stokbroden * stokbrodenaantal ) - (kortingsbonnen * korting))
+if antwoord == (18.88):
+    print("blablabla" + str(croissantjes) + "blablabla2") 
+print (antwoord) 
+
+
+
+
+
+
---
+++ speelhaldagvariabel.py
@@ -0,0 +1,7 @@
+tickets = 3
+ticketaantal = int(input("hoeveel tickets heb je?"))
+vrbrilminuten = int(input("hoeveel minuten neem je?")) 
+x = int((tickets * 7.45) + (vrbrilminuten/5 * 0.37))
+print (x) 
+if x == (25.68):
+    print ("geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro")
