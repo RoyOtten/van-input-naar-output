3fc182125668da1e137e6e4bb962785d33df2962
RoyOtten <99065498@mydavinci.nl>
2021-09-08 09:57:27 +0200

    eerste commit

---
+++ feestlunch.py
@@ -0,0 +1,13 @@
+croissantjes = 0.39
+aantal = 17 
+stokbroden = 2.78
+aantallen = 2
+korting = 0.50
+aantalbonnen = 3 
+antwoord = (croissantjes * aantal) + (stokbroden * aantallen ) - (aantalbonnen * korting)
+if antwoord == (18.88):
+    print("blablabla" + str(croissantjes) + "blablabla2") 
+print (antwoord) 
+
+ 
+
